2085cd7eebe854d8dd3e524820ba8cc8aaca9c1e
wilmer3004 <131203913+wilmer3004@users.noreply.github.com>
2023-10-15 20:31:47 -0500

    se creo el proyecto2-RelojDigital

---
+++ Proyecto2/script.js
@@ -0,0 +1,25 @@
+function calcularTiempo(){
+    let tiempo = new Date();
+
+    let hora = tiempo.getHours();
+    let minuto = tiempo.getMinutes();
+    let segundo = tiempo.getSeconds();
+    
+    hora = hora < 10 ? "0" + hora : hora;
+    minuto = minuto < 10 ? "0" + minuto : minuto;
+    segundo = segundo < 10 ? "0" + segundo : segundo;
+
+    let pantallaReloj = hora + ":" + minuto + ":" + segundo;
+    let relojDigital = document.querySelector(".reloj")
+    relojDigital.innerHTML = pantallaReloj;
+    
+
+}
+
+setInterval(calcularTiempo, 1000)
+
+
+
+
+
+
